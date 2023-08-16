def06d6ce4c4cfdec3b5b94b902a2aa37be81a04
Aziz Zeghal <101135665+Tofeha@users.noreply.github.com>
2023-08-16 18:56:09 +0000

    Replaced Selenium with Bs4

--- ArpejChecker.py
+++ ArpejChecker.py
@@ -1,41 +1,27 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
 from time import sleep
-
-#--| Setup
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
-
-url = "https://www.arpej.fr/fr/residence/porte-ditalie-residence-etudiante-le-kremlin-bicetre/"
-driver.get(url)
-sleep(1)
+import requests
 
 def call():
     found = False
-    max_retries = 10
     retries = 0 
     
     while not found and retries < max_retries:
         #First, we try to check if there is NO accomodation
-        try:
-            acc = driver.find_element(By.XPATH, "//p[@class='widget-ibail__desc widget-ibail__desc--over']")
-            print("Nothing")
-        #If we cannot find the container, an accomodation is there !
-        except NoSuchElementException:
-            #Add code to notify user
+        html_content = requests.get(url).text
+
+        soup = BeautifulSoup(html_content,"html.parser")
+
+        target_class = "widget-ibail__desc widget-ibail__desc--over"
+        target = soup.find_all(class_ = target_class)
+        
+        if target == []:
             print("Accommodation here!")
             found = True
-            driver.quit()
             return 0
-        else:
-            retries += 1
-            sleep(10)
-            driver.refresh()
-
-    print("Accommodation not found or maximum retries reached.")
-    driver.quit()
+        
+        retries += 1
+        sleep(300)
 
 call()
+
